095e975cfae3a3bc36c79f1b348ae7ca2dc7dec9
Iq-Life <iq23life@gmail.com>
2021-09-17 22:20:34 +0300

    Homework #3 Checkbox

--- src/p2-homeworks/h4/common/c2-SuperButton/SuperButton.tsx
+++ src/p2-homeworks/h4/common/c2-SuperButton/SuperButton.tsx
@@ -6,21 +6,34 @@ type DefaultButtonPropsType = DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonE
 
 type SuperButtonPropsType = DefaultButtonPropsType & {
     red?: boolean
+    title:string
 }
 
 const SuperButton: React.FC<SuperButtonPropsType> = (
     {
-        red, className,
+        red, className, title,
         ...restProps// все остальные пропсы попадут в объект restProps, там же будет children
     }
 ) => {
-    const finalClassName = `${red ? s.red : s.default} ${className}`
+
+    const finalClassNameButton = `${red ? s.red : s.button} ${className}`
+    const finalClassNameSpanTop= red ? `${s.red_button_line_top} ${s.red_button_line}` : `${s.button_line_top} ${s.button_line}`
+    const finalClassNameSpanRight= red ? `${s.red_button_line_right} ${s.red_button_line}` : `${s.button_line_right} ${s.button_line}`
+    const finalClassNameSpanBottom= red ? `${s.red_button_line_bottom} ${s.red_button_line}` : `${s.button_line_bottom} ${s.button_line}`
+    const finalClassNameSpanLeft= red ? `${s.red_button_line_left} ${s.red_button_line}` : `${s.button_line_left} ${s.button_line}`
+
+
 
     return (
-        <button
-            className={finalClassName}
+        <button className={finalClassNameButton}
             {...restProps} // отдаём кнопке остальные пропсы если они есть (children там внутри)
-        />
+        >
+            <span className={finalClassNameSpanTop}/>
+            <span className={finalClassNameSpanRight}/>
+            <span className={finalClassNameSpanBottom}/>
+            <span className={finalClassNameSpanLeft}/>
+            {title}
+        </button>
     )
 }
 
