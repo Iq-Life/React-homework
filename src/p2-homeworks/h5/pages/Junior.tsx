10a1c9b97853b7281fb3c28f2c36516e5178a183
Iq-Life <iq23life@gmail.com>
2021-10-08 10:12:12 +0300

    Homework #8

--- src/p2-homeworks/h5/pages/Junior.tsx
+++ src/p2-homeworks/h5/pages/Junior.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import s from "../../../p1-main/m1-ui/u1-app/App.module.css";
 import HW7 from "../../h7/HW7";
+import HW8 from "../../h8/HW8";
 
 function Junior() {
     return (
@@ -9,6 +10,8 @@ function Junior() {
             <ul className={s.ul}>
                 <h4>Homework #7</h4>
                 <li><HW7/></li>
+                <h4>Homework #8</h4>
+                <li><HW8/></li>
 
             </ul>
         </div>
